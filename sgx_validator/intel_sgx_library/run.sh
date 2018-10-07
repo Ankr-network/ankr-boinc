839e6e2021202237d0bc96819055ce0d28661185
yousongzhang <yousong@ankr.network>
2018-10-06 22:27:48 -0700

    [SWDEV-51] add openssl lib path

--- sgx_validator/intel_sgx_library/run.sh
+++ sgx_validator/intel_sgx_library/run.sh
@@ -2,5 +2,5 @@ g++ -DHAVE_CONFIG_H -I.  -I/opt/intel/sgxsdk/include -fno-builtin-memset -I/opt/
 ar srv sgx_boinc_sp.a sgx_boinc_sp.o agent_wget.o iasrequest.o byteorder.o common.o crypto.o hexutil.o fileio.o base64.o msgio.o logfile.o agent_curl.o
 
 g++ -std=c++11  -g  -L/opt/intel/sgxsdk/lib64 -L/opt/openssl/1.1.0i/lib -o test sgx_boinc_call_remote_attestation_test.cpp   sgx_boinc_sp.a -lcrypto -lcurl  -I/opt/intel/sgxsdk/include
-
+export LD_LIBRARY_PATH=/opt/openssl/1.1.0i/lib
 ./test
